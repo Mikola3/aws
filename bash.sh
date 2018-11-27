1e4fa6cd904d63d5e9ed08a406a9da55a4afcce0
Mikola3 <34712864+Mikola3@users.noreply.github.com>
2018-11-27 15:58:07 +0300

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,1 @@
+aws --region us-east-1 cloudformation describe-stack-resource --stack-name pkislouski-mira36 --query ‘StackResourceDetail.PhysicalResourceId’ --output text --logical-resource-id DNS Z31OJE14P8IWKC
