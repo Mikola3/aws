7cf37063e835d14398790df6c9892043357abe9d
Mikola3 <34712864+Mikola3@users.noreply.github.com>
2018-11-27 16:02:53 +0300

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,1 +1,4 @@
-aws --region us-east-1 cloudformation describe-stack-resource --stack-name pkislouski-mira36 --query ‘StackResourceDetail.PhysicalResourceId’ --output text --logical-resource-id DNS Z31OJE14P8IWKC
+# parse stack
+aws --region us-east-1 cloudformation describe-stack-resource --stack-name my-stack-name --query 'StackResourceDetail.PhysicalResourceId' --output text --logical-resource-id DNS Z31OJE14P8IWKC
+# create stack
+aws --region us-east-1 cloudformation create-stack --stack-name my-stack-name --template-body file://stack.yaml --capabilities CAPABILITY_IAM --parameters ParameterKey=KeyName,ParameterValue=mykey
