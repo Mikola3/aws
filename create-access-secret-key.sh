4ad9355cf4ce84cb7fa16b7d691e91d4c3a7b704
Mikola3 <34712864+Mikola3@users.noreply.github.com>
2019-01-28 14:52:24 +0300

    Update create-access-secret-key.sh

--- create-access-secret-key.sh
+++ create-access-secret-key.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 AccessKey=$(aws iam create-user --user-name ${AWS::StackName} | jq -r '.User | .UserId')
 SecretKey=$(aws iam create-access-key --user-name ${AWS::StackName} | jq -r '.AccessKey | .SecretAccessKey')
+# attach policy on the created user
 aws iam attach-user-policy --policy-arn arn:aws:iam::aws:policy/AmazonSQSFullAccess --user-name ${AWS::StackName}
