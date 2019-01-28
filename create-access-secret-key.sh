6651bcba85efdb49113039313aee0e0d66c94ead
Mikola3 <34712864+Mikola3@users.noreply.github.com>
2019-01-28 14:43:22 +0300

    Update create-access-secret-key.sh

--- create-access-secret-key.sh
+++ create-access-secret-key.sh
@@ -2,5 +2,3 @@
 AccessKey=$(aws iam create-user --user-name ${AWS::StackName} | jq -r '.User | .UserId')
 SecretKey=$(aws iam create-access-key --user-name ${AWS::StackName} | jq -r '.AccessKey | .SecretAccessKey')
 aws iam attach-user-policy --policy-arn arn:aws:iam::aws:policy/AmazonSQSFullAccess --user-name ${AWS::StackName}
-# further use of variables
-docker run --name=fcm -d -e ACCESS_KEY=$AccessKey -e SECRET_KEY=$SecretKey docker_image
